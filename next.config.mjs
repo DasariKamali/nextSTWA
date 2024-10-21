c07f04885500266c1f3ce90616eb41036819bb5b
Dasari Kamali (LTIMINDTREE LIMITED) <v-dkamali@microsoft.com>
2024-10-21 19:47:22 +0530

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/blog',
+};
+
+export default nextConfig;
